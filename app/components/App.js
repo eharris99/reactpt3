3b8aeefa44bce571b5114ce447e0519d5e9b08c0
Elise Harris <eharris99@yahoo.com>
2016-09-19 22:35:10 -0400

    first real commit

---
+++ app/components/App.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var App = React.createClass({
+  render: function() {
+    
+    return(<div>
+      <h1>yakkety yak</h1>
+    </div>)
+  }
+})
+
+module.exports = App;
